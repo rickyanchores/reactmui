e0f1fbd0a5c271932c0c5ffb525aa1be53fb6bf4
RickyAnchores <ricky23anchores@gmail.com>
2022-06-08 17:24:52 +0100

    mixin scss

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,9 +8,11 @@ const Home = () => {
                 
             </div>
             <div className="text">
+
                 <h1>Home</h1>
                 <Typography variant="h6">this is a test react material-ui</Typography>
                 <Button variant='contained'>Click Here</Button>
+                         
             </div>
             <img src="https://images.unsplash.com/photo-1653778005824-4bc7dc887603?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1015&q=80" alt="" />
         </div>
